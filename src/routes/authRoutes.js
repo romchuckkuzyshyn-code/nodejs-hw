ccce1f821feecddfa06d7019e23b0e67d6d26232
Roman <romchuck.kuzyshyn@gmail.com>
2026-05-25 11:08:10 +0300

    fixed auth Routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -13,9 +13,9 @@ import {
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), registerUser);
-router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-router.post('/auth/logout', logoutUser);
-router.post('/auth/refresh', refreshUserSession);
+router.post('/register', celebrate(registerUserSchema), registerUser);
+router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/logout', logoutUser);
+router.post('/refresh', refreshUserSession);
 
 export default router;
